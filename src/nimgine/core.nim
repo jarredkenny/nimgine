3c0aad53461ac55c5bd270e8044137af6e297b1e
Jarred Kenny <jarred.kenny@screenscape.com>
2019-10-07 00:16:10 -0300

    Progress on engine loop

--- src/nimgine/core.nim
+++ src/nimgine/core.nim
@@ -1,59 +1,9 @@
-import opengl/glut
-import opengl
-import opengl/glu
+import platform
+import ecs
 
-include clock
-include ecs
+platform.init()
 
-var clock: Clock = newClock()
+proc loop(dt: float) =
+  echo "Game Loop"
 
-proc reshape(width: GLsizei, height: GLsizei) {.cdecl.} =
-  if height == 0:
-    return
-  echo "Resized: " & $width & "x" & $height
-  glViewport(0, 0, width, height)
-  glMatrixMode(GL_PROJECTION)
-  glLoadIdentity()
-  gluPerspective(45.0, width / height, 0.1, 100.0)
-
-proc display() {.cdecl.} =
-  glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-  glMatrixMode(GL_MODELVIEW)
-  glLoadIdentity()
-  glTranslatef(1.5, 0.0, -7.0)
-  glBegin(GL_TRIANGLES)
-  glEnd()
-  glutSwapBuffers()
-
-proc loop() {.cdecl.} =
-  update(clock)
-
-proc init*() =
-  # Glut Setup
-  var argc: cint = 0
-  glutInit(addr argc, nil)
-  glutInitDisplayMode(GLUT_DOUBLE)
-  glutInitWindowSize(640, 480)
-  glutInitWindowPosition(50, 50)
-
-  # Create Window
-  discard glutCreateWindow("Nimgine")
-
-  # Setup callbacks
-  glutDisplayFunc(display)
-  glutReshapeFunc(reshape)
-  glutIdleFunc(loop)
-
-  # Load Extensions
-  loadExtensions()
-
-  # Init GL State
-  glClearColor(0.0, 0.0, 0.0, 1.0) # Black
-  glClearDepth(1.0)
-  glEnable(GL_DEPTH_TEST)
-  glDepthFunc(GL_LEQUAL)
-  glShadeModel(GL_SMOOTH)
-  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
-
-  # Enter GLUT main loop
-  glutMainLoop()
+platform.setGameLoopFunc(loop)
--- src/nimgine/ecs.nim
+++ src/nimgine/ecs.nim
@@ -1,20 +1,23 @@
-import tables, typetraits
+import tables, typetraits, sugar
 
 type
-  Component = ref object
-    id: int
+  Component* = ref object
+    id*: int
 
-  Entity = ref object
-    id: int
-    components: Table[string, Component]
+  Entity* = ref object
+    id*: int
+    components*: Table[string, Component]
 
-  System = ref object
-    id: int
+  System* = ref object
+    id*: int
+    init*: proc(system: System)
+    update*: proc(system: System, dt: float)
+    render*: proc(system: System)
 
-  World = ref object
-    id: int
-    entities: seq[Entity]
-    systems: seq[System]
+  World* = ref object
+    id*: int
+    entities*: seq[Entity]
+    systems*: seq[System]
 
 # Id counters
 var entityCount: int = 0
@@ -22,29 +25,33 @@ var componentCount: int = 0
 var systemCount: int = 0
 var worldCount: int = 0
 
+
 # Component Functions
 proc `$`(c: Component): string =
   result = "<Component id=" & $c.id & ">"
 
-proc newComponent(): Component =
+proc newComponent*(): Component =
   inc(componentCount)
   result = Component(id: componentCount)
 
 # Entity Functions
-proc newEntity(): Entity =
+proc newEntity*(): Entity =
   inc(entityCount)
   result = Entity(id: entityCount)
 
-proc add[T](entity: Entity, component: T) =
+proc add*[T](entity: Entity, component: T) =
   if not entity.components.hasKey(name(T)):
     entity.components.add(name(T), component)
 
-proc get(entity: Entity, T: typedesc): T =
+proc get*(entity: Entity, T: typedesc): T =
   if entity.components.hasKey(name(T)):
     result = entity.components[name(T)]
 
 # System Functions
-proc newSystem(): System =
+proc `$`(system: System): string =
+  result = "<System id=" & $system.id & ">"
+
+proc newSystem*(): System =
   inc(systemCount)
   result = System(id: systemCount)
 
@@ -53,26 +60,27 @@ proc `$`(w: World): string =
   result = "<World entities=" & $len(w.entities) & " systems=" & $len(
       w.systems) & ">"
 
-proc newWorld(): World =
+proc newWorld*(): World =
   inc(worldCount)
   result = World(id: worldCount)
 
-proc add(world: World, system: System) =
+proc add*(world: World, system: System) =
   world.systems.add(system)
 
-proc add(world: World, entity: Entity) =
+proc add*(world: World, entity: Entity) =
   world.entities.add(entity)
 
-# Testing
-var world: World = newWorld()
-var system: System = newSystem()
-var e1: Entity = newEntity()
-var c1: Component = newComponent()
+proc init*(world: World) =
+  for system in world.systems:
+    if system.init != nil:
+      system.init(system)
 
-e1.add(c1)
+proc update*(world: World, dt: float) =
+  for system in world.systems:
+    if system.update != nil:
+      system.update(system, dt)
 
-world.add(e1)
-world.add(system)
-
-echo(c1)
-echo(world)
+proc render*(world: World) =
+  for system in world.systems:
+    if system.render != nil:
+      system.render(system)
